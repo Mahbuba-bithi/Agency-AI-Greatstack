5f50c05ec71389adfce8371b7f03c1a72f4688dd
Mahbuba Bithi <mahbubabithi0email@gmail.com>
2026-05-20 15:10:36 +0600

    redeploy fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar"
-
 import Hero from "./components/Hero"
 import TrustedBy from "./components/TrustedBy"
 import Services from "./components/Services"
